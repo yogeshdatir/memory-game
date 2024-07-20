4658fdfeb263bdb127b4e767eff792219cdbe683
Yogesh Datir <yogdat@gmail.com>
2024-07-20 15:59:35 +0530

    updated eslint config as per rec from vite readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +17,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
   },
 };
